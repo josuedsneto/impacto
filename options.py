8197882983075feb50ed0a59ae75e1a4336a701a
josuedsneto <josuedsneto@gmail.com>
2026-02-22 16:15:15 -0300

    Remove deprecated options.py file as functionality has been migrated to individual pages.

--- options.py
+++
@@ -1,3 +0,0 @@
-# This file is kept only for git history.
-# All functionality has been split into individual pages under pages/.
-# Streamlit will not show this file as a page because it has no st.set_page_config call.
